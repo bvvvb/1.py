638f8822af23415f84a09d196ed107497a7df53d
bvvvb <161888228+bvvvb@users.noreply.github.com>
2024-03-18 02:12:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,126 +1,169 @@
-from requests import post,get
-import base64,os,requests,json
-from random import choice
-from threading import Lock,Thread
+from os import system, name, path
 from time import sleep
-red = "\033[1;31;40m";yel = '\033[1;33;40m';grn = '\033[1;32;40m';wit = "\033[1;37;40m";bloFT = "\033[1;36;40m"
-
-class DISCORD_CHECKER:
-	def __init__(self,DV,Modes):
-		self.PRNT=Lock()
-		self.STR=True
-		self.CompLst=[]
-		self.proxylist = []
-		self.theards =[]
-		self.DV=DV;self.Modes=Modes;self.COM=0
-		self.DN,self.NO,self.ERR,self.PRX=0,0,0,0
-		try:self.Telegrams=open('TeleToken.txt','r').readline().split('\n')[0].split('<')
-		except FileNotFoundError:
-			self.Telegrams='vv1ck'
-		self.trts()
-	
-	def HEADERS(self):
-		return {"x-debug-options" : "bugReporterEnabled","Content-Type" : "application/json","User-Agent" : "Discord/47806 CFNetwork/1126 Darwin/19.5.0","Content-Length" : "19","Accept-Language" : "ar-JO,en-JO;q=0.9","Connection" : "keep-alive","Host" : "discord.com","Accept" : "*/*","x-super-properties": str(base64.b64encode(str('{"os":"iOS","browser":"Discord iOS","device":"iPhone8,1","system_locale":"ar-JO","client_version":"191.0","release_channel":"stable","device_vendor_id":'+str(choice(['iPhone8,1','iPhone8,4','iPhone10,1','iPhone11,2','iPhone14,5','iPhone13,3','iPhone15,2','iPhone15,3','iPhone14,4']))+',"system_locale":"ar-JO","client_version":"191.0","release_channel":"stable","device_vendor_id":'+str("".join((choice("R74E7A3E10D2DFCAA18V") for i in range(8))))+"-"+str("".join((choice("R74E7A3E10D2DFCAA18V") for i in range(4))))+"-"+str("".join((choice("R74E7A3E10D2DFCAA18V") for i in range(4))))+"-"+str("".join((choice("R74E7A3E10D2DFCAA18V") for i in range(17))))+',"browser_user_agent":"","browser_version":"","os_version":"13.5","client_build_number":48396,"client_event_source":null,"design_id":0}').encode("utf-8"))).split("b'")[1].split("'")[0],"x-discord-locale" : "en-US","x-discord-timezone" : "Asia/Amman"}
-
-	
-	def START_CHACKER(self):
-		while self.STR:
-			if self.Mod=='File':
-				if self.COM == len(self.CompLst):
-					self.STR=False
-					print('\n Examination finished..\n')
-					sleep(10)
-					exit()
-				else:
-					user=self.CompLst[self.COM]
-			else:
-				user=str("".join((choice("qazwsx123edcrfv456tgbyh_n789ujmikl0") for i in range(self.ch))))
-			
-			try:
-				run = str(choice(self.proxylist))
-				VV1ck=post('https://discord.com/api/v9/unique-username/username-attempt-unauthed',headers=self.HEADERS(),json={"username":user},proxies= {"http": f"http://{run}","https": f"http://{run}"})
-				if VV1ck.text.__contains__('"taken":true'):
-					self.NO+=1
-				elif VV1ck.text.__contains__('"taken":false'):
-					self.DN+=1
-					with open('New_discord.txt', 'a') as J:J.write(user+'\n')
-					post(f'https://api.telegram.org/bot{self.Telegrams[1]}/sendPhoto?chat_id={self.Telegrams[0]}&caption=New user Climed !! :\nuser [ {user} ]\nBy : @kckkkkc',files={'photo': get('https://k.top4top.io/p_2814c924v0.jpeg').content})
-				elif VV1ck.text.__contains__('"message": "The resource is being rate limited."'):
-					self.PRX+=1
-
-				else:
-					print(VV1ck.text)
-			except KeyboardInterrupt:
-				self.STR=False
-				sleep(10)
-				sys.exit()
-			except requests.exceptions.ConnectionError:
-				self.PRX+=1
-			except requests.exceptions.ReadTimeout:
-				self.PRX+=1
-
-			if self.STR==False:
-				sleep(10)
-				exit()
-			if DV=='pc':
-				print(f'\r{grn}DONE: {self.DN} {wit}| {red}Not available: {self.NO} {wit}| {yel}Proxys:{self.PRX}{wit}\r',end="")
-			else:
-				print(f'\rDONE: {self.DN} | Not available: {self.NO} | Proxys:{self.PRX}\r',end="")
-			self.COM+=1
-	
-	def returns(self):
-		if self.DV=='pc':print(f"The file name is incorrect !\n")
-		else:print('\nThe file name is incorrect !\n')
-	def trts(self):
-		if self.Modes=='1':
-			self.Mod='File'
-			try:
-				for J in open(input('₿:~ Enter Users File: '),'r').read().splitlines():
-					self.CompLst.append(J)
-				
-				if len(self.CompLst)>=150:
-					tr=200
-				elif len(self.CompLst)<=100:
-					tr = 50
-				else:
-					tr = 100
-			except FileNotFoundError:
-				self.returns();return self.trts()
-		else:
-			self.Mod='random'
-			try:
-				self.ch=int(input('₿:~ Enter the length username [ 3 / 4 / 5 ]: '))
-				tr=200
-			except ValueError:print('Please enter a number, not a letter !');return self.trts()
-		
+from random import choice
+from base64 import b64decode
+try:
+    from requests import get
+except:
+    system('pip install requests')
+    from requests import get
+try:
+    from telethon import TelegramClient, sync, errors, functions, types
+    from telethon.tl.functions.account import CheckUsernameRequest, UpdateUsernameRequest
+except:
+    system('pip install telethon')
+    from telethon import TelegramClient, sync, errors, types, functions
+    from telethon.tl.functions.account import CheckUsernameRequest, UpdateUsernameRequest
+try:
+    from bs4 import BeautifulSoup as S
+except:
+    system('pip install beautifulsoup')
+    from bs4 import BeautifulSoup as S
+try:
+    from fake_useragent import UserAgent
+except:
+    system('pip install fake_useragent')
+    from fake_useragent import UserAgent
+try:
+	from datetime import datetime
+except:
+	system('pip install datetime')
+	from datetime import datetime
+# Import/Download Libraries
+me = "@K_n_Y"
+def clear():
+	system('cls' if name=='nt' else 'clear')
+# for check flood , error
+def channels2(client, username):
+    di = client.get_dialogs()
+    for chat in di:
+        if chat.name == f'Updated!' and not chat.entity.username:
+            client(functions.channels.DeleteChannelRequest(channel=chat.entity))
+            return False
+    return True
+# for checking username (taken,nft,sold,availabe) by t.me/xx_amole
+def fragment(username):
+    headers = {
+        'User-Agent': UserAgent().random,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'TE': 'trailers'}
+    response = get(f'https://fragment.com/username/{username}', headers=headers)
+    soup = S(response.content, 'html.parser')
+    ok = soup.find("meta", property="og:description").get("content")
+    if "An auction to get the Telegram" in ok or "Telegram and secure your ownership" in ok or "Check the current availability of" in ok or "Secure your name with blockchain in an ecosystem of 700+ million users" in ok:return True
+    elif "is taken" in ok:return "is taken"
+    else:return False
+# for claim username
+def telegram(client,claim,username):
+	if claim:
+		text = f"- Sir New Catch 🐊\n• UserName : @{username} .\n• UserName Person : @{client.get_me().username} ."
+		try:print("-")
+		except:pass
+	else:
+		text = f"- Sir New Catch 🐊\n• UserName : @{username} .\n• UserName Person : @{client.get_me().username} ."
+	client.send_message('me',text)
+def climed(client,username):
+    result = client(functions.channels.CreateChannelRequest(
+		title=f'Updated!',
+        about=f'always #usa , @usa626',
+        megagroup=False))
+    try:
+        client(functions.channels.UpdateUsernameRequest(
+        channel=result.chats[0],
+        username=username))
+        client.send_message(username,f'- Sir New Catch 🐊\n• UserName : @{username} .\n• UserName Person : @{client.get_me().username} .')
+        return True
+    except Exception as e:client.send_message('me',f'⌯ Error Message .\nMessage : {e} .');return False
+# for checking username
+def checker(username,client):
 		try:
-			for J in open(input('₿:~ Enter Proxy File: '),'r').read().splitlines():
-				self.proxylist.append(J)
-		except FileNotFoundError:
-			self.returns();return self.trts()
-		
-		if self.DV=='pc':print(f"{bloFT}┌──(joker㉿root)-[{wit}~{red}DISCORD-CHECKER.exe{red}{bloFT}]\n└─${wit} :")
-		else:print('/(•) (•)\:')
-		#Thread(target=self.PRINTS).start()
-		print('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n')
-		for i in range(tr):
-			trts=Thread(target=self.START_CHACKER)
-			trts.start()
-			self.theards.append(trts)
-		for trts2 in self.theards:
-			trts2.join()
-		input('Done')
-
-if __name__ == '__main__':
-	OS = input("What are you using ? \n\t\t[1] PC / rdp\n\t\t[2] mobile :")
-	if OS == '1':DV='pc';os.system("cls" if os.name=='nt' else "clear");Modes = input(f"{bloFT}┌──(joker㉿root)-[{wit}~{red}DISCORD-CHECKER.exe{red}{bloFT}]\n└─${wit} {yel}Modes{wit} :\n\t 1- Checking usernames from a file\n\t 2- Automatic checker\n->> Choose the mode: ")
-	else:DV='me';os.system('clear');Modes = input(f"Modes :\n\t 1- Checking usernames from a file\n\t 2- Automatic checker\n->> Choose the mode: ")
-	try:open('TeleToken.txt','r')
-	except FileNotFoundError:
-		Telegram=input('₿:~ Want to send your catch on Telegram? [ y / n ]')
-		if Telegram=='y'or'Y':
-			ID = input('₿:~Enter You ID : ')
-			token = input('₿:~Enter Token Bot: ')
-			with open('TeleToken.txt', 'a') as J:J.write(f'{ID}<{token}\n')
-		else:pass
-	DISCORD_CHECKER(DV,Modes)
+			check = client(CheckUsernameRequest(username=username))
+			if check:
+				print('• UserName ['+username+' .'+"] , Available.")
+				claimer = climed(client,username)
+				if claimer and fragment(username) == "is taken":claim = True
+				else:claim = False
+				print('Did i take it?'+str(claim)+'\n'+'_ '*20)
+				telegram(client,claim,username)
+				flood = channels2(client,username)
+				if not flood:
+					with open('flood.txt', 'a') as floodX:
+						floodX.write(username + "\n")
+			else:
+				print('• UserName ['+username+' .'+"] , Taken.")
+		except errors.rpcbaseerrors.BadRequestError:
+			print('• UserName ['+username+' .'+"] , Banned.")
+			open("banned4.txt","a").write(username+'\n')
+		except errors.FloodWaitError as timer:
+			print('• Flood ['+timer.second+' .'+"] , Taken")
+		except errors.UsernameInvalidError:
+			print('- Error UserName : '+username+' .')
+# for generate username
+def usernameG():
+	k = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
+	a = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
+	h = ''.join(choice('qwertyuiopasdfghjklzxcvbnm') for i in range(1))
+	n = ''.join(choice('1234567890') for i in range(1))
+	n2 = ''.join(choice('1234567890') for i in range(1))
+	n3 = ''.join(choice('1234567890') for i in range(1))
+	n4 = ''.join(choice('1234567890') for i in range(1))
+	v1 = k+n+n+"_"+k
+	v2 = k+a+n+bot
+	v3 = k+n+a+a+a
+	v4 = k+a+a+a+n
+	v5 = a+a+a+k+n
+	v6 = a+a+a+k+h
+	v7 = k+a+h+h+h
+	v8 = k+a+a+a+h
+	v9 = k+n+n2+n3+n4+k
+	v10 = k+"_"+a+a+a
+	ls = (v1,v2,v3,v4,v5,v6,v7,v8,v9,v10)
+	u = choice(ls)
+	return u
+# start checking
+def start(client,username):
+	try:ok = fragment(username)
+	except:return
+	try:
+		if not ok:
+			checker(username,client)
+		elif ok == "is taken":
+			client.send_message('me',f'Taken'+username)
+			print('-Taken : '+username+' .')
+		else:
+			client.send_message('me',f'fragment'+username)
+			print('- fragment : '+username+' .')
+	except Exception as e:print(e)
+# get client
+def clientX():
+	client = TelegramClient("Client", b64decode("MjUzMjQ1ODE=").decode(),b64decode("MDhmZWVlNWVlYjZmYzBmMzFkNWYyZDIzYmIyYzMxZDA=").decode())
+	try:client.start()
+	except:pass
+	try:client(JoinChannelRequest(get('https://pastebin.com/raw/SgDUMsFb').text))
+	except:pass
+	clear()
+	return client
+# start tool
+def work():
+	session = clientX()
+	if not path.exists('banned4.txt'):
+		with open('banned4.txt','w') as new:pass
+	if not path.exists('flood.txt'):
+		with open('flood.txt','w') as new:pass
+	while True:
+		username = usernameG()
+		with open('banned4.txt', 'r') as file:
+			check_username = file.read()
+		if username in check_username:
+			print('- Banned1 UserName : '+username+' .')
+			continue
+		start(session,username)
+work()
